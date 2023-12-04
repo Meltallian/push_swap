caf930329f549a1fd5c5d449e13cacef7e0174b7
Jeremie Bidaux <jbidaux@c1r11s7.42lausanne.ch>
2023-12-04 12:54:34 +0100

    base

---
+++ Makefile
@@ -0,0 +1,39 @@
+# Variables
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+NAME = push_swap
+LIBFT = libft/libft.a
+PUSH_SWAP_SRC = main.c
+PUSH_SWAP_OBJ = $(PUSH_SWAP_SRC:.c=.o)
+#INCLUDES_DIR = -I./includes -I./libft/includes -I./minilibx_macos/includes
+#pas necessaire parceque -lft -lmlx dans la ligne cc
+# Default rule
+all: $(NAME)
+
+# This won't be called if the files don't exist or are not updated
+$(PUSH_SWAP_OBJ): %.o: %.c
+	$(CC) $(CFLAGS) $(INCLUDES_DIR) -c $< -o $@
+
+$(LIBFT):
+	make -C ./libft
+
+$(NAME): $(LIBFT) $(PUSH_SWAP_OBJ)
+	$(CC) $(PUSH_SWAP_OBJ) $(LIBFT) -o $(NAME) -L./libft -lft
+
+clean:
+	make clean -C ./libft
+	rm -f $(PUSH_SWAP_OBJ)
+
+fclean: clean
+	make fclean -C ./libft
+	rm -f $(NAME)
+
+re: fclean all
+
+run: $(NAME)
+	./$(NAME)
+
+leaks: $(NAME)
+	leaks --atExit -- ./$(NAME)
+
+.PHONY: all clean fclean re run
---
+++ push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbidaux <jeremie.bidaux@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 12:47:51 by jbidaux           #+#    #+#             */
+/*   Updated: 2023/12/04 12:48:35 by jbidaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <fcntl.h>
+# include <string.h>
+# include <limits.h>
+# include "libft/libft.h"
+
+#endif // PUSH_SWAP_H
